0818853cc25f92b6dad3a14f8bc515c504d9067b
Daniel Santos <daniel@codein.ca>
2020-06-20 02:22:03 -0400

    configure firebase emulator function and storage

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,18 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+admin.initializeApp();
+// Authentication => identity.
+// Authorization => permissions.
+export const yt = functions.https.onRequest(async (request, response) => {
+  const key1 = request.body.key1 || `No "key1" was received.`;
+  const fullBody = JSON.stringify(request.body);
+  const firestoreInstance = await admin.firestore();
+  const firestoreAdd = await firestoreInstance
+    .collection('youtube-queries')
+    .add({ key1: key1, fullBody: fullBody });
+  // const saveQueryToFirebase = await admin.firestore().collection('youtube-queries').add({query: requestText});
+  response.json({
+    result: `Key #1: ${key1} -- Firestore key: ${firestoreAdd.id}`,
+  });
 });
