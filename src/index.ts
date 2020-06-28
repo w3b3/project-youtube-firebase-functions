65656656378a30f140758395172520adf170eb8d
Daniel Santos <daniel@codein.ca>
2020-06-27 22:21:21 -0400

    treat firebase.config.get as async

--- src/index.ts
+++ src/index.ts
@@ -5,39 +5,68 @@ admin.initializeApp();
 require('dotenv').config();
 
 const got = require('got').default; //Error if not CommonJS https://github.com/axios/axios#note-commonjs-usage
-const BASE_URL = process.env.DEV_BASE_URL ? process.env.DEV_BASE_URL : functions.config().api.base_url;
+
+const BASE_URL = async () => {
+  let url = '';
+  if (process.env.DEV_BASE_URL) {
+    url = process.env.DEV_BASE_URL;
+  } else {
+    // url = functions.config().api.base_url;
+    url = await functions.config().api.base_url;
+    console.debug(url);
+  }
+  return url;
+};
+
 const rand = () => Math.trunc(Math.random() * 100); //TODO: remove
 
-export const receiveQueryAndFirestoreIt = functions.https.onRequest(async (request, response) => {
+export const receiveQueryAndFirestoreIt = functions.https.onRequest(
+  async (request, response) => {
+    const _url = await BASE_URL();
     const q = request.body.q;
     if (q) {
-        // Save to storage
-        await admin.firestore().collection('queries')
-            .add({data: {...q}, _id: rand()});
-        // Return response to client
-        response.json({q});
+      // Save to storage
+      await admin
+        .firestore()
+        .collection('queries')
+        .add({ data: { ...q }, _id: `${_url} - ${rand()}` });
+      // Return response to client
+      response.json({ q });
     } else {
-        response.status(400).end();
+      response.status(400).end();
     }
-});
+  }
+);
 
 // Authentication => identity.
 // Authorization => permissions.
-export const retrieveSubscriptionsForUser = functions.https.onRequest(async (request, response) => {
+export const retrieveSubscriptionsForUser = functions.https.onRequest(
+  async (request, response) => {
+    const _url = await BASE_URL();
     //TODO: authenticate user
     const userId = await Promise.resolve(`test-${rand()}`);
     //TODO: get user ID
     if (userId) {
-        //TODO: call Youtube API to get list of subscriptions
-        const subscriptions = [rand(), rand()];
-        // save the list of subscriptions in Firestore
-        const r: { userId: string; subscriptions: number[] } = {userId: userId, subscriptions};
-        got(`${BASE_URL}receiveQueryAndFirestoreIt`, {
-            method: 'POST', json: {q: r}, responseType: 'json'
+      //TODO: call Youtube API to get list of subscriptions
+      const subscriptions = [rand(), rand()];
+      // save the list of subscriptions in Firestore
+      const r: { userId: string; subscriptions: number[]; error?: any } = {
+        userId: userId,
+        subscriptions,
+      };
+      try {
+        got(`${_url}receiveQueryAndFirestoreIt`, {
+          method: 'POST',
+          json: { q: r },
+          responseType: 'json',
         });
-        // TODO: clean up and return list of subscriptions to client
-        response.json(r);
+      } catch (e) {
+        r.error = e;
+      }
+      // TODO: clean up and return list of subscriptions to client
+      response.json({ r });
     } else {
-        response.status(400).end();
+      response.status(400).end();
     }
-});
+  }
+);
