2c71d36d9c02eab8b30754cb10f75a20a22a9a49
Daniel Santos <daniel@codein.ca>
2020-06-30 14:16:08 -0400

    minor aesthetic updates. more:
    - credentials are passed from ENV, not reading JSON
    - retrieve "mine" videos
    - replace var with const
    - IDE auto adds inferred types

--- src/youtube.ts
+++ src/youtube.ts
@@ -1,26 +1,27 @@
+require('dotenv').config();
+
 // below https://developers.google.com/youtube/v3/quickstart/nodejs
-var fs = require('fs');
-var readline = require('readline');
-var { google } = require('googleapis');
-var OAuth2 = google.auth.OAuth2;
+let fs = require('fs');
+let readline = require('readline');
+let { google } = require('googleapis');
+let OAuth2 = google.auth.OAuth2;
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/youtube-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
-var TOKEN_DIR =
+let SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
+let TOKEN_DIR =
   (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) +
   '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
+let TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
 
-// Load client secrets from a local file.
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the YouTube API.
-  authorize(JSON.parse(content), getChannel);
-});
+//NOTE: Hard coding credentials in my local ENV
+const CREDENTIALS = {
+  clientSecret: process.env.clientSecret,
+  clientId: process.env.clientId,
+  redirectUrl: process.env.redirectUrl,
+};
+// Authorize a client with the loaded credentials, then call the YouTube API.
+authorize(CREDENTIALS, getChannel);
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -29,14 +30,17 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+function authorize(
+  credentials: { clientSecret: any; clientId: any; redirectUrl: any },
+  callback: { (auth: any): void; (arg0: any): void }
+) {
+  const clientSecret = credentials.clientSecret;
+  const clientId = credentials.clientId;
+  const redirectUrl = credentials.redirectUrl;
+  const oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
 
   // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function (err, token) {
+  fs.readFile(TOKEN_PATH, function (err: boolean, token: string) {
     if (err) {
       getNewToken(oauth2Client, callback);
     } else {
@@ -54,17 +58,24 @@ function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback to call with the authorized
  *     client.
  */
-function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
+function getNewToken(
+  oauth2Client: {
+    generateAuthUrl: (arg0: { access_type: string; scope: string[] }) => any;
+    getToken: (arg0: any, arg1: (err: any, token: any) => void) => void;
+    credentials: any;
+  },
+  callback: { (auth: any): void; (arg0: any): void; (arg0: any): void }
+) {
+  const authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
   });
   console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
+  const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-  rl.question('Enter the code from that page here: ', function (code) {
+  rl.question('Enter the code from that page here: ', function (code: any) {
     rl.close();
     oauth2Client.getToken(code, function (err, token) {
       if (err) {
@@ -83,7 +94,7 @@ function getNewToken(oauth2Client, callback) {
  *
  * @param {Object} token The token to store to disk.
  */
-function storeToken(token) {
+function storeToken(token: any) {
   try {
     fs.mkdirSync(TOKEN_DIR);
   } catch (err) {
@@ -91,7 +102,7 @@ function storeToken(token) {
       throw err;
     }
   }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err: any) => {
     if (err) throw err;
     console.log('Token stored to ' + TOKEN_PATH);
   });
@@ -102,20 +113,20 @@ function storeToken(token) {
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function getChannel(auth) {
-  var service = google.youtube('v3');
+function getChannel(auth: any) {
+  const service = google.youtube('v3');
   service.channels.list(
     {
       auth: auth,
       part: 'snippet,contentDetails,statistics',
-      forUsername: 'GoogleDevelopers',
+      mine: true,
     },
-    function (err, response) {
+    function (err: string, response: { data: { items: any } }) {
       if (err) {
         console.log('The API returned an error: ' + err);
         return;
       }
-      var channels = response.data.items;
+      const channels = response.data.items;
       if (channels.length == 0) {
         console.log('No channel found.');
       } else {
